b2f42eb2e93079274cbf5ee6468bfd4eff48b67a
fleple <davidyushkov@gmail.com>
2017-07-04 18:22:37 +0300

    yoyo alfa verion ~0.2

--- src/components/static/MainPage.js
+++ src/components/static/MainPage.js
@@ -3,8 +3,26 @@ import React from 'react'
 const MainPage = (props) => (
   <div>
     <h1>Welcome, body ~(^.^)~</h1>
-    <h2>alpha version ~0.01</h2>
-    <img alt='alf' src='./imgs/alf2.gif'/>
+    <section>
+      <h2>alpha version ~0.02</h2>
+      <img alt='alf02' src='./imgs/alf_alfa02.gif'/>
+      <ol>
+        <li>country and city filter</li>
+        <li>offset humans</li>
+        <li>count humans</li>
+      </ol>
+    </section>
+    <hr/>
+    <section>
+      <h2>alpha version ~0.01</h2>
+      <img alt='alf01' src='./imgs/alf_alfa01.gif'/>
+      <ol>
+        <li>only vk</li>
+        <li>search</li>
+        <li>vote</li>
+        <li>result</li>
+      </ol>
+    </section>
   </div>
 )
 
